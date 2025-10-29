4f09f077a88f5d1899ab4b6c147be48fbda43f8b
JoelA510 <joela510@gmail.com>
2025-10-28 23:00:13 -0700

    test(jest): add setupTests and .env.test; ensure router for CRA tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,6 @@
 import '@testing-library/jest-dom';
-process.env.REACT_APP_SUPABASE_URL ||= 'http://localhost:54321';
-process.env.REACT_APP_SUPABASE_ANON_KEY ||= 'test-anon-key';
+
+process.env.REACT_APP_SUPABASE_URL =
+  process.env.REACT_APP_SUPABASE_URL || 'http://localhost:54321';
+process.env.REACT_APP_SUPABASE_ANON_KEY =
+  process.env.REACT_APP_SUPABASE_ANON_KEY || 'test_anon_key';
