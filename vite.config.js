dcb7b1f42dee1dac0deaf51a3274168b87c4818d
JoelA510 <joela510@gmail.com>
2026-01-10 19:44:36 -0800

    refactor(hygiene): remove utils folder, update gitignore, document lint debt

--- vite.config.js
+++ vite.config.js
@@ -19,7 +19,6 @@ export default defineConfig({
             '@layouts': path.resolve(__dirname, './src/layouts'),
             // Stashed aliases (for compatibility with new Planter components)
             '@': path.resolve(__dirname, './src'),
-            'utils': path.resolve(__dirname, './src/utils'),
             'lib': path.resolve(__dirname, './src/lib'),
             'api': path.resolve(__dirname, './src/api'),
             'entities': path.resolve(__dirname, './src/entities'),
